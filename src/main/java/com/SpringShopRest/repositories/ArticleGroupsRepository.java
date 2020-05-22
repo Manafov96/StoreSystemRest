5de3b9402da55e9d33574391084dddd483102176
Manafov96 <viko9687@gmail.com>
2020-05-22 17:10:13 +0300

    Change article groups repository.

--- src/main/java/com/SpringShopRest/repositories/ArticleGroupsRepository.java
+++ src/main/java/com/SpringShopRest/repositories/ArticleGroupsRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ArticleGroupsRepository extends CrudRepository<ArticleGroups, Long> {
 
-    @Query(nativeQuery = true, value = "select RCG.ID, RCG.NAME_DE, RCG.NAME_BG, RCG.NAME_EN from REST_CATEGORIES_GET(?) RCG")
+    @Query(nativeQuery = true, value = "select RCG.ID, RCG.NAME_DE, RCG.NAME_BG, RCG.NAME_EN from REST_ARTICLES_GROUPS_GET(?) RCG")
     Iterable<ArticleGroups> findAll(Integer processed);
 }
